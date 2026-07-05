2039739c19f9c40eccd4edba11ecf9856d817323
amxrac <108365444+amxrac@users.noreply.github.com>
2026-07-05 07:02:02 +0100

    port

--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello, world!")
 	})
-
-	log.Println("Listening on :8000")
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
